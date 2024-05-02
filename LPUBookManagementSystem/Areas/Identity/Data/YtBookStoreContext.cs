7c0d84546dc8e2f2221246a0442e26eae579482e
Abhijais2003 <jjaiswal.aj.1234@gmail.com>
2024-05-02 15:32:42 +0530

    Add project files.

---
+++ LPUBookManagementSystem/Areas/Identity/Data/YtBookStoreContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using YtBookStore.Areas.Identity.Data;
+
+namespace YtBookStore.Data;
+
+public class YtBookStoreContext : IdentityDbContext<YtBookStoreUser>
+{
+    public YtBookStoreContext(DbContextOptions<YtBookStoreContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
